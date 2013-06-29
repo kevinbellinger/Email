a3cfb6dc9d09414a751bd94d5fffc4801cee53db
Liangjun Jiang <ljiang510@gmail.com>
2013-06-29 14:38:29 -0500

    move files around

--- pull_dependencies.sh
+++ pull_dependencies.sh
@@ -4,9 +4,6 @@
 # - git: http://help.github.com/mac-git-installation/
 # - mercurial / hg: http://mercurial.berkwood.com/
 
-# pull three20
-cd ..
-git clone git://github.com/facebook/three20.git 
 
 # pull mailcore - new URL
 git clone git://github.com/mronge/MailCore.git
