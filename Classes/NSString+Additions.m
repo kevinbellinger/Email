aaa29fe17ec1bc8bf125492e288c85dbaad56c8e
Liangjun Jiang <ljiang510@gmail.com>
2013-06-29 14:35:01 -0500

    remove three20

---
+++ Classes/NSString+Additions.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Additions.m
+//  ReMailIPhone
+//
+//  Created by LIANGJUN JIANG on 6/29/13.
+//
+//
+
+#import "NSString+Additions.h"
+
+@implementation NSString (Additions)
+
+@end
